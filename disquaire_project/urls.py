f8217181047ef908463ce99a943f358c0bfa7765
taoufik51 <49036109+taoufik51@users.noreply.github.com>
2022-03-08 18:21:32 +0100

    Add files via upload

---
+++ disquaire_project/urls.py
@@ -0,0 +1,41 @@
+"""disquaire_project URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.conf import settings
+from django.urls import include, re_path
+from django.contrib import admin
+
+
+
+from store import views
+
+
+
+urlpatterns = [
+
+     re_path(r'^$', views.index, name='index'),
+     #re_path(r'^post/', include(('store.urls','store'), namespace='store')),
+     re_path(r'^store/', include('store.urls', namespace='store')),
+     re_path(r'^hassioui/', admin.site.urls)
+]
+
+
+
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        re_path(r'^__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
